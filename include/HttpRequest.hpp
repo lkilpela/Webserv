e656c0eb05f67bb4417e944557f9491ca022a13c
hoangfin <hoang.tran.fin@gmail.com>
2024-12-04 01:02:21 +0200

    Working on extracting headers from raw request

--- include/HttpRequest.hpp
+++ include/HttpRequest.hpp
@@ -6,18 +6,25 @@
 class HttpRequest {
 	private:
 		std::string _method;
-    	std::string _url;
+    	std::string _uri;
+    	std::string _version;
     	std::unordered_map<std::string, std::string> _headers;
     	std::string _body;
+		bool _isCgi;
+		bool _isDirectory;
 
 	public:
 		HttpRequest(const std::string& rawRequest);
 		HttpRequest(const HttpRequest& request);
-		~HttpRequest();
+		~HttpRequest() = default;
 		HttpRequest& operator=(const HttpRequest& request);
 
 		const std::string& getMethod() const;
-		const std::string& getUrl() const;
+		const std::string& getUri() const;
+		const std::string& getVersion() const;
 		const std::unordered_map<std::string, std::string>& getHeaders() const;
 		const std::string& getBody() const;
+
+		bool isDirectory() const;
+		bool isCgi() const;
 };
---
+++ src/http/HttpRequest.cpp
@@ -0,0 +1,22 @@
+#include <sstream>
+#include "HttpRequest.hpp"
+
+HttpRequest::HttpRequest(const std::string& rawRequest) {
+	std::istringstream stream(rawRequest);
+	std::string line;
+
+	std::getline(stream, line);
+	std::istringstream requestLine(line);
+	requestLine >> _method >> _uri >> _version;
+}
+// HttpRequest::HttpRequest(const HttpRequest& request);
+// HttpRequest::HttpRequest& operator=(const HttpRequest& request);
+
+const std::string& HttpRequest::getMethod() const { return _method; }
+const std::string& HttpRequest::getUri() const { return _uri; }
+const std::string& HttpRequest::getVersion() const { return _version; }
+const std::unordered_map<std::string, std::string>& HttpRequest::getHeaders() const { return _headers; }
+const std::string& HttpRequest::getBody() const { return _body; }
+
+bool HttpRequest::isDirectory() const { return _isDirectory; }
+bool HttpRequest::isCgi() const { return _isCgi; }
