40ad8093a9c1253bdd24c9c983f32fffc19d6e83
Hoang Tran <hoangtran.fin@gmail.com>
2024-12-18 19:46:23 +0200

    Revising Response class for statefulness

--- include/http/Response.hpp
+++ include/http/Response.hpp
@@ -10,21 +10,19 @@ namespace http {
 
 	class Response {
 		private:
-			int _clientSocket;
-			http::Status _status { http::Status::Code::ACCEPTED };
+			http::Status _status { Status::Code::ACCEPTED };
 			std::string _type;
 			std::string _url;
 			std::string _body;
 			std::unordered_map<std::string, std::string> _headers;
-			std::size_t _totalBytesSent { 0 };
-			bool _isRedirected { false };
+			std::size_t _bytesSent { 0 };
 			bool _isComplete { false };
 
 			std::string _composeHeaders() const;
-			void _send(const void *buf, const size_t size, int flags);
+			void _send(int clientSocket, const void *buf, const size_t size, int flags);
 
 		public:
-			Response(int clientSocket);
+			Response() = default;
 			Response(const Response& response);
 			~Response() = default;
 
@@ -36,9 +34,9 @@ namespace http {
 			Response& setHeader(Header header, const std::string& value);
 			Response& setBody(const std::string& bodyContent);
 
-			void sendFile(const std::string& filePath);
-			void sendStatus(http::Status status);
-			void sendText(const std::string& text);
+			void sendFile(int clientSocket, const std::string& filePath);
+			void sendStatus(int clientSocket, Status status);
+			void sendText(int clientSocket, const std::string& text);
 
 			bool isComplete() const;
 	};
--- src/http/Response.cpp
+++ src/http/Response.cpp
@@ -19,8 +19,6 @@
 
 namespace http {
 
-	Response::Response(int clientSocket) : _clientSocket(clientSocket) {}
-
 	// Response::Response(const Response& response)
 	// 	: _statusCode(response._statusCode)
 	// 	, _statusText(response._statusText)
@@ -42,7 +40,9 @@ namespace http {
 	// 	return *this;
 	// }
 
-	const Status& Response::getStatus() const { return _status; }
+	const Status& Response::getStatus() const {
+		return _status;
+	}
 
 	Response& Response::setStatus(const Status& status) {
 		_status = status;
@@ -75,33 +75,35 @@ namespace http {
 		return stream.str();
 	}
 
-	void Response::_send(const void *buf, const size_t size, int flags) {
-		const ssize_t bytesSent = ::send(_clientSocket, buf + _totalBytesSent, size - _totalBytesSent, flags);
+	void Response::_send(int clientSocket, const void *buf, const size_t size, int flags) {
+		const ssize_t bytesSent = ::send(clientSocket, buf + this->_bytesSent, size - this->_bytesSent, flags);
 		if (bytesSent < 0) {
 			// throw SocketException("Failed to send data");
+			_lastSent = std::chrono::stead_clock::now();
 		}
-		_totalBytesSent += static_cast<std::size_t>(bytesSent);
-		if (_totalBytesSent >= size) {
+		this->_bytesSent += static_cast<std::size_t>(bytesSent);
+		if (this->_bytesSent >= size) {
 			_isComplete = true;
+			this->_bytesSent = 0;
 		}
 	}
 
-	void Response::sendFile(const std::string& filePath) {
+	void Response::sendFile(int clientSocket, const std::string& filePath) {
 		std::ifstream file(filePath);
 
-		
+
 	}
 
-	void Response::sendStatus(Status status) {
+	void Response::sendStatus(int clientSocket, Status status) {
 		setStatus(status);
 		setHeader(Header::CONTENT_TYPE, "text/plain");
 		setHeader(Header::CONTENT_LENGTH, std::to_string(_status.reason.length()));
 
 		std::string response = _composeHeaders() + _status.reason;
-		_send(response.c_str(), response.size(), 0);
+		_send(clientSocket, response.c_str(), response.size(), 0);
 	}
 
-	void Response::sendText(const std::string& text) {
+	void Response::sendText(int clientSocket, const std::string& text) {
 
 	}
 
