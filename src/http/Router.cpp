514aa657c7e62e6f8ee526f4f5ac780ef62954f0
lumikil <kilpelainenlumi@gmail.com>
2025-01-03 12:34:09 +0200

    Refactor Router methods to accept Location config directly and enhance error handling with detailed logging

--- src/http/Router.cpp
+++ src/http/Router.cpp
@@ -1,11 +1,24 @@
-#include "Router.hpp"
+#include "Config.hpp"
+#include "http/Router.hpp"
+#include "Error.hpp"
 
 namespace http {
 	void Router::get(const Location& config, Handler handler) {
-		//_routes[route]["GET"] = handler;
+		std::cout << "Router::get request received" << std::endl;
+
+		// Log the config details
+		std::cout << YELLOW "Log config details" RESET << std::endl;
+        std::cout << "Config path: " << config.path << std::endl;
+        std::cout << "Config methods: ";
+        for (const auto& method : config.methods) {
+            std::cout << method << " ";
+        }
+        std::cout << std::endl;
+
 		// Check if the GET in the config.methods
 		if (std::find(config.methods.begin(), config.methods.end(), "GET") == config.methods.end()) {
-			throw RouterError("GET method not allowed for this location");
+			std::cerr << "GET method not allowed for this location" << std::endl;
+			throw RouterError(EINVAL, "GET method not allowed for this location");
 		}
 		// If allowed, add to route 
 		_routes[config.path]["GET"] = handler;
@@ -13,22 +26,24 @@ namespace http {
 	}
 
 	void Router::post(const Location& config, Handler handler) {
+		std::cout << "Router::post request received" << std::endl;
 		if (std::find(config.methods.begin(), config.methods.end(), "POST") == config.methods.end()) {
-			throw RouterError("POST method not allowed for this location");
+			throw RouterError(EINVAL, "POST method not allowed for this location");
 		}
 		_routes[config.path]["POST"] = handler;
 		_locationConfigs[config.path] = config;
 	}
 
 	void Router::del(const Location& config, Handler handler) {
+		std::cout << "Router::del request received" << std::endl;
 		if (std::find(config.methods.begin(), config.methods.end(), "DELETE") == config.methods.end()) {
-			throw RouterError("DELETE method not allowed for this location");
+			throw RouterError(EINVAL, "DELETE method not allowed for this location");
 		}
 		_routes[config.path]["DELETE"] = handler;
 		_locationConfigs[config.path] = config;
 	}
 
-	const Location* findBestMatchingLocation(const std::string& url) const {
+	const Location* Router::findBestMatchingLocation(const std::string& url) const {
 		const Location* bestMatch = nullptr;
 		size_t longestMatch = 0;
 
@@ -66,11 +81,14 @@ namespace http {
 		// Find the best matching LocationConfig for the requested route
 		const Location* location = findBestMatchingLocation(route);
 
+		ServerConfig serverConfig;
+
 		// No matching location found, return http status 404
 		if (!location) {
+			std::cout << "Location Not found" << std::endl;
 			response
 				.setStatusCode(StatusCode::NOT_FOUND_404)
-				.setBody("Route not found")
+				.setBody(std::make_unique<utils::FilePayload>(0, serverConfig.errorPages[404])) // need to fix this
 				.build();
 			return;
 		}
@@ -85,22 +103,24 @@ namespace http {
 
 			// The matched route does not support the requested http method, return http status 405
 			if (handlerByMethodIt == handlerByMethod.end()) {
+				std::cout << "Method not allowed" << std::endl;
 				response
 					.setStatusCode(StatusCode::METHOD_NOT_ALLOWED_405)
-					.setBody("Method not allowed")
+					.setBody(nullptr) // need to fix this
 					.build();
 				return;
 			}
 
 			// Execute the handler, return http status 500 if an exception occurs
 			try {
+				std::cout << "Handler found" << std::endl;
 				const auto handler = handlerByMethodIt->second;
 				handler(request, response);
 			} catch(const std::exception& e) {
 				response
 					.clear()
 					.setStatusCode(StatusCode::INTERNAL_SERVER_ERROR_500)
-					.setBody("Internal server error")
+					.setBody(nullptr) // need to fix this
 					.build();
 			}
 
