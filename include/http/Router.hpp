d5ebd2cba0e56b3e61fa1e13346373abe9e7dcd4
lumikil <kilpelainenlumi@gmail.com>
2025-01-03 21:16:58 +0200

    Refactor Router to support simplified handler registration and improve location management

--- include/http/Router.hpp
+++ include/http/Router.hpp
@@ -9,16 +9,19 @@
 namespace http {
 	class Router {
 		public:
-			using Handler = std::function<void(Request&, Response&)>;
+			using Handler = std::function<void(const Location&, Request&, Response&)>;
 
-			void get(const Location& config, Handler handler);
-			void post(const Location& config, Handler handler);
-			void del(const Location& config, Handler handler);
+			void get(Handler handler);
+			void post(Handler handler);
+			void del(Handler handler);
 			void handle(Request& req, Response& res);
+
+			void addLocations(const ServerConfig& serverConfig);
 
 
 		private:
-			std::unordered_map<std::string, std::unordered_map<std::string, Handler>> _routes; //route -> method -> handler
+			ServerConfig _serverConfig;
+			std::unordered_map<std::string, Handler> _routes; //route -> method -> handler
 			std::unordered_map<std::string, Location> _locationConfigs; // route -> location config
 
 			const Location* findBestMatchingLocation(const std::string& url) const;
