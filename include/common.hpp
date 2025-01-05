f33cf57a49f747b700e1a4b4a922a6e2df794cee
lumikil <kilpelainenlumi@gmail.com>
2025-01-06 01:47:10 +0200

    Refactor utils namespace to config and add convertSizeToBytes function for size conversion

--- include/common.hpp
+++ include/common.hpp
@@ -6,19 +6,19 @@
 #include <algorithm>
 #include <iterator>
 
-namespace utils {
-    std::string trim(const std::string &str);
+namespace config {
+	std::string trim(const std::string &str);
 	std::string trimSpace(const std::string& str);
-    std::string removeComments(const std::string &str);
+	std::string removeComments(const std::string &str);
 	std::string lowerCase(std::string str);
-    bool parseBool(const std::string &value);
-    int parsePort(const std::string &value);
-    void validateMethods(const std::vector<std::string> &methods);
-    bool isValidIP(const std::string &ip);
-    bool isValidFilePath(const std::string &path);
-    bool isValidURL(const std::string &url);
-    bool isValidSize(const std::string &size);
-    void validateErrorPage(const std::string &code, const std::string &path);
+	bool parseBool(const std::string &value);
+	int parsePort(const std::string &value);
+	void validateMethods(const std::vector<std::string> &methods);
+	bool isValidIP(const std::string &ip);
+	bool isValidFilePath(const std::string &path);
+	bool isValidURL(const std::string &url);
+	bool isValidSize(const std::string &size);
+	void validateErrorPage(const std::string &code, const std::string &path);
 
 	template <typename Iterator>
 	Iterator findDelimiter(
@@ -28,4 +28,5 @@ namespace utils {
 	) {
 		return std::search(begin, end, delimiter.begin(), delimiter.end());
 	}
+	std::size_t convertSizeToBytes(const std::string& sizeStr);
 }
