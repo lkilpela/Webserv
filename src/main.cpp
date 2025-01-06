991ae15ef8d4c41c2db5e962c871736c3cd110b5
lumikil <kilpelainenlumi@gmail.com>
2025-01-06 13:00:03 +0200

    Enhance file path validation and update error handling; refactor Config structure for multiple CGI extensions

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ int main(int argc, char **argv) {
 		//Server server(config);
 		//server.listen();
 	} catch (const WSException& e) {
-		std::cerr << "Error: " << e.code() << " " << e.code().message() << std::endl;
+		std::cerr << "Error: " << e.code() << " " << e.what() << std::endl;
 	} catch (const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 		return 1;
