c2aed8cfe33719b1bbc39bc969e9ce76c460d30a
lumikil <kilpelainenlumi@gmail.com>
2025-01-07 13:59:25 +0200

    git checkout Server files

--- include/utils/common.hpp
+++ include/utils/common.hpp
@@ -7,26 +7,20 @@
 #include <iterator>
 
 namespace utils {
-	std::string trim(const std::string &str);
+    std::string trim(const std::string &str);
 	std::string trimSpace(const std::string& str);
-	std::string removeComments(const std::string &str);
+    std::string removeComments(const std::string &str);
 	std::string lowerCase(std::string str);
-	bool parseBool(const std::string &value);
-	int parsePort(const std::string &value);
-	void validateMethods(const std::vector<std::string> &methods);
-	bool isValidIP(const std::string &ip);
-	bool isValidFilePath(const std::string &path);
-	bool isValidURL(const std::string &url);
-	bool isValidSize(const std::string &size);
-	void validateErrorPage(const std::string &code, const std::string &path);
-	int setNonBlocking(int fd);
-	void closeFDs(const std::vector<int> &fds);
-
-	//isInVector
-	template <typename T>
-	bool isInVector(const T &value, const std::vector<T> &vec) {
-		return std::find(vec.begin(), vec.end(), value) != vec.end();
-	}
+    bool parseBool(const std::string &value);
+    int parsePort(const std::string &value);
+    void validateMethods(const std::vector<std::string> &methods);
+    bool isValidIP(const std::string &ip);
+    bool isValidFilePath(const std::string &path);
+    bool isValidURL(const std::string &url);
+    bool isValidSize(const std::string &size);
+    void validateErrorPage(const std::string &code, const std::string &path);
+	void closeFDs(const std::vector<int>& serverFds);
+	std::size_t convertSizeToBytes(const std::string& sizeStr);
 
 	template <typename Iterator>
 	Iterator findDelimiter(
@@ -36,5 +30,9 @@ namespace utils {
 	) {
 		return std::search(begin, end, delimiter.begin(), delimiter.end());
 	}
-	std::size_t convertSizeToBytes(const std::string& sizeStr);
+
+	template <typename T>
+	bool isInVector(const T& element, const std::vector<T>& vec) {
+    	return std::find(vec.begin(), vec.end(), element) != vec.end();
+	}
 }
