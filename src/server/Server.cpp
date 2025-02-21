06ea015bc7764e60ab3c6625c9bfb6db3b800416
michael1nguyen1 <michael22nguyen@gmail.com>
2025-02-21 16:51:23 +0200

    redid getENV

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -120,58 +120,50 @@ void Server::_closePipeFd(int fd) {
 	}
 }
 
-// char **makeEnv(char** &env, http::Request& req){
+std::vector<char*> makeEnv(http::Request& req){
 
-// 	http::Url url = req.getUrl();
-// 	char **res = new char*[9];
+	http::Url url = req.getUrl();
+	std::vector<char*> res;
 
-// 	res[0] = new char[url.scheme.size() + 1];
-// 	res[1] = new char[url.user.size() + 1];
-// 	res[2] = new char[url.password.size() + 1];
-// 	res[3] = new char[url.host.size() + 1];
-// 	res[4] = new char[url.port.size() + 1];
-// 	res[5] = new char[url.path.size() + 1];
-// 	res[6] = new char[url.query.size() + 1];
-// 	res[7] = new char[url.fragment.size() + 1];
-// 	res[8] = nullptr;
+	res.push_back(const_cast<char*>(req.getUrl().scheme.c_str()));
+	res.push_back(const_cast<char*>(req.getUrl().user.c_str()));
+	res.push_back(const_cast<char*>(req.getUrl().password.c_str()));
+	res.push_back(const_cast<char*>(req.getUrl().host.c_str()));
+	res.push_back(const_cast<char*>(req.getUrl().port.c_str()));
+	res.push_back(const_cast<char*>(req.getUrl().path.c_str()));
+	res.push_back(const_cast<char*>(req.getUrl().query.c_str()));
+	res.push_back(const_cast<char*>(req.getUrl().fragment.c_str()));
+	res.push_back(nullptr);
 
-// 	return res;
-// }
+	return res;
+}
 
-// void deleteEnv(char **env){
-// 	for (int i = 0; env[i]; i++){
-// 		delete[] env[i];
-// 	}
-// 	delete[] env;
-// }
+void Server::_cgiHandler(http::Request &req, http::Response &res) {
 
-// void Server::_cgiHandler(http::Request &req, http::Response &res) {
-
-// 	char* interpreter = "/usr/bin/python3";
-// 	// if (access(interpreter, X_OK) == -1 || access(script, X_OK) == -1){}
-// 		//return 403 forbidding
-// 	const char* script = req.getUrl().path.c_str();
-// 	char* scriptArray[2] = {const_cast<char*>(script), nullptr};
-// 	int pipefd[2];
-// 	if(pipe(pipefd) == -1)
-// 		perror("Pipe failed");
-// 	if (utils::setNonBlocking(pipefd[0]) == false)
-// 		perror("Pipe nonblocking failed");
-// 	pollfd cgiData { pipefd[0], POLLIN, 0 };
-// 	_pollfds.push_back(cgiData);
-// 	pid_t pid = fork();
-// 	if (pid == 0){
-// 		close(pipefd[0]);
-// 		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-// 			perror("Dup2 failed");
-// 		char **env = makeEnv(env, req);
-// 		execve(interpreter, scriptArray, env);
-// 		deleteEnv(env);
-// 	}
-// 	pid_t result = waitpid(pid, NULL, WNOHANG);
-// 	if (result = pid)
-// 		close(pipefd[0]);
-// }
+	char* interpreter = "/usr/bin/python3";
+	// if (access(interpreter, X_OK) == -1 || access(script, X_OK) == -1){}
+		//return 403 forbidding
+	const char* script = req.getUrl().path.c_str();
+	char* scriptArray[2] = {const_cast<char*>(script), nullptr};
+	int pipefd[2];
+	if(pipe(pipefd) == -1)
+		perror("Pipe failed");
+	if (utils::setNonBlocking(pipefd[0]) == false)
+		perror("Pipe nonblocking failed");
+	pollfd cgiData { pipefd[0], POLLIN, 0 };
+	_pollfds.push_back(cgiData);
+	pid_t pid = fork();
+	if (pid == 0){
+		close(pipefd[0]);
+		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+			perror("Dup2 failed");
+		std::vector<char*> env = makeEnv(req);
+		execve(interpreter, scriptArray, env.data());
+	}
+	pid_t result = waitpid(pid, NULL, WNOHANG);
+	if (result = pid)
+		close(pipefd[0]);
+}
 
 // void Server::_read(struct ::pollfd& pollFd, http::Connection& con) {
 // 	if (pollFd.revents & POLLIN == 0) {
